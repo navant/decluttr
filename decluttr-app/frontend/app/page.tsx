302b482ad0edd748d52c96c9cde5a8e7a9995f27
Michal <michalwnt@gmail.com>
2023-12-19 09:48:58 +1100

    added the image upload func

--- decluttr-app/frontend/app/page.tsx
+++ decluttr-app/frontend/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/app/components/header";
+import ImageUpload from "./components/ImageUpload";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center gap-10 p-24 background-gradient">
       <Header />
+      <ImageUpload />
     </main>
   );
 }
