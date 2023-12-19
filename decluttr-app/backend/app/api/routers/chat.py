0f416746dee718e398909de70f16d9ad57b85109
IsisChameleon <12711084+IsisChameleon@users.noreply.github.com>
2023-12-19 06:00:56 +0000

    Fixing chat.py

--- decluttr-app/backend/app/api/routers/chat.py
+++ decluttr-app/backend/app/api/routers/chat.py
@@ -6,7 +6,7 @@ from app.utils.json import json_to_model
 from app.utils.index import get_index
 from fastapi import APIRouter, Depends, HTTPException, Request, status
 from llama_index import VectorStoreIndex
-from llama_index.llms.base import MessageRole, ChatMessage
+from llama_index.llms import MessageRole, ChatMessage
 from pydantic import BaseModel
 
 chat_router = r = APIRouter()
